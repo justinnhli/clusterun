ec4a7f6286d446638e5787cca5caeb3a897c68ce
Justin Li <justinnhli@gmail.com>
2019-10-31 11:17:06 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='clusterun',
-    version='0.0.2b9',
+    version='0.0.2b10',
     description='A script to run jobs on a Torque server',
     url='https://github.com/justinnhli/clusterun',
     author='Justin Li',
